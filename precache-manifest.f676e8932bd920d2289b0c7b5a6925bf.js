e3b98248b39f77eab51d73462a8b8f4a0dbc9c9f
Vitor Falcao <joaofalcao.33@outlook.com>
2022-08-29 11:45:37 -0300

    Updates

---
+++ precache-manifest.f676e8932bd920d2289b0c7b5a6925bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8160cac5909b604d988",
+    "url": "/use-context-03/static/js/main.c8160cac.chunk.js"
+  },
+  {
+    "revision": "24758a8b680d33a928be",
+    "url": "/use-context-03/static/js/1.24758a8b.chunk.js"
+  },
+  {
+    "revision": "ddd0059fc398f0ecc923",
+    "url": "/use-context-03/static/js/runtime~main.ddd0059f.js"
+  },
+  {
+    "revision": "527eda5933d6bec80c2076ff3cadd43a",
+    "url": "/use-context-03/index.html"
+  }
+];
